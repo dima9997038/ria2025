e90123fc870259fa46b783f602906ced0e8b85e3
dima9997038 <dima9997038@gmail.com>
2025-01-27 23:09:54 +0300

    Email

--- src/components/GetStartedComponent.js
+++ src/components/GetStartedComponent.js
@@ -40,35 +40,35 @@ function GetStartedComponent({ email,setEmail }) {
     return (
         <section id="get-started" className="get-started section-bg">
             <div className="container">
-                {/*<Row className="row justify-content-between gy-4">*/}
-                {/*    <Col className="col-lg-6 d-flex align-items-center" data-aos="fade-up">*/}
-                {/*        <div className="content">*/}
-                {/*            <h3>Ждем Ваших сообщений. Каждое Ваше обращение или предложение очень важно для нас.</h3>*/}
-                {/*            <p> Мы благодарны за каждое Ваше обращение или отзыв о нашей работе </p>*/}
-                {/*        </div>*/}
-                {/*    </Col>*/}
-                {/*    <Col className="col-lg-5" data-aos="fade">*/}
-                        <Form ref={form} className="php-email-form" onSubmit={sendEmail}>
+                <Row className="row justify-content-between gy-4">
+                    <Col className="col-lg-6 d-flex align-items-center" data-aos="fade-up">
+                        <div className="content">
+                            <h3>Ждем Ваших сообщений. Каждое Ваше обращение или предложение очень важно для нас.</h3>
+                            <p> Мы благодарны за каждое Ваше обращение или отзыв о нашей работе </p>
+                        </div>
+                    </Col>
+                    <Col className="col-lg-5" data-aos="fade">
+                        <Form ref={form} className="rounded p-4 p-sm-3" onSubmit={sendEmail}>
                             <h3>Заполните пожалуста форму и отправьте Ваше сообщение.</h3>
                             <p>В соответствии с Законом об обработке персональных данных при отправке нам сообщение Вы даете разреешение на обработку персональных данных </p>
-                            <Row className="row gy-3 justify-content-between">
-                                <Col className="col-md-12">
+                            {/*<Row className="row gy-3 justify-content-between">*/}
+                                <Form.Group>
                                     <Form.Control type="text" name="user_name" className="form-control " placeholder="Ваше имя и фамилия"
                                                   required></Form.Control>
-                                </Col>
-                                <Col className="col-md-12">
+                                </Form.Group>
+                                <Form.Group className="col-md-12">
                                     <Form.Control type="email" name="user_email" className="form-control" placeholder="Email" value={email} onChange={changeEmail}
                                                   required></Form.Control>
-                                </Col>
-                                <Col className="col-md-12">
+                                </Form.Group>
+                                <Form.Group className="col-md-12">
                                     <Form.Control type="text" name="phone" className="form-control" placeholder="Телефон"
                                                   required></Form.Control>
-                                </Col>
-                                <Col className="col-md-12">
+                                </Form.Group>
+                                <Form.Group className="col-md-12">
                                     <Form.Control as="textarea" name="message" className="form-control" rows="6"
                                                   placeholder="Сообщение" required></Form.Control>
-                                </Col>
-                                <Col className="col-md-12 text-center">
+                                </Form.Group>
+                                <Form.Group className="col-md-12 text-center">
                                     <div className="loading">Отправка</div>
                                     <div className="error-message"></div>
                                     <div className="sent-message">Ваш запрос был успешно отправлен. Спасибо!
@@ -79,15 +79,15 @@ function GetStartedComponent({ email,setEmail }) {
                                         ):<></>
 
                                     }
-                                </Col>
-                                <Col className="col-md-12 text-center">
+                                </Form.Group>
+                                <Form.Group className="col-md-12 text-center">
 
                                     <Button href="/" variant={"warning"} >Вернуться на главную страницу</Button>
-                                </Col>
-                            </Row>
+                                </Form.Group>
+                            {/*</Row>*/}
                         </Form>
-                {/*    </Col>*/}
-                {/*</Row>*/}
+                    </Col>
+                </Row>
             </div>
         </section>
     );
