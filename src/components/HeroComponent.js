37222eb6f1d53dca6f312b9fcf5ee3f62ff503d4
dima9997038 <dima9997038@gmail.com>
2025-02-02 17:25:28 +0300

    fix

--- src/components/HeroComponent.js
+++ src/components/HeroComponent.js
@@ -4,17 +4,24 @@ import background6 from "../img/hero-carousel/church.png";
 import background7 from "../img/hero-carousel/avenue.png";
 import background8 from "../img/hero-carousel/river.png";
 import background9 from "../img/hero-carousel/victory.png";
+// import "./Hero.css"
 
 function HeroComponent(props) {
     return (
         <section id="hero" className="hero">
-
             <div className="info d-flex align-items-center">
                 <div className="container">
                     <div className="row justify-content-center">
-                        <div className="col-lg-6 text-center">
-                            <h3 style={{color:"yellow"}} data-aos="fade-down">Официальный торговый представитель в  Беларуси - ООО&nbsp;«КАМА», АО&nbsp;«ГОЗНАК» ЦБК&nbsp;г.Краснокамск, АО&nbsp;«Волга», ЦБК&nbsp;«Кондопога»</h3>
-                            <p data-aos="fade-up"><h2>Ваш поставщик бумаги и картона с 1993 года</h2></p>
+                        <div className="col-sm-6 col-lg-6 col-xl-6  text-center">
+                            <h3
+                                style={{color:"yellow"}}
+                                data-aos="fade-down">
+                                Официальный торговый представитель в  Беларуси - ООО&nbsp;«КАМА», АО&nbsp;«ГОЗНАК» ЦБК&nbsp;г.Краснокамск, АО&nbsp;«Волга», ЦБК&nbsp;«Кондопога»
+                            </h3>
+                            <div data-aos="fade-up">
+                                <h2
+                                >Ваш поставщик бумаги и картона с 1993 года
+                                </h2></div>
                             {/*<a data-aos="fade-up" data-aos-delay="200" href="#projects"*/}
                             {/*   className="btn-get-started">Купить сейчас</a>*/}
                         </div>
@@ -33,13 +40,10 @@ function HeroComponent(props) {
                 <a className="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
                     <span className="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
                 </a>
-
                 <a className="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
                     <span className="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
                 </a>
-
             </div>
-
         </section>
     );
 }
